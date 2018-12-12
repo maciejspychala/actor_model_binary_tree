be9f1cf54e6aead001fd3935aaffb3d76f032bbb
maciejspychala <mac.spychala@gmail.com>
2018-12-12 20:31:50 +0100

    add loop for testing

--- tree.erl
+++ tree.erl
@@ -1,5 +1,5 @@
 -module(tree).
--export([nd/1, insert/2]).
+-export([nd/1, insert/2, loop/0]).
 
 -record(node, {val=nil,
                left=nil,
@@ -12,3 +12,13 @@ insert(V, nil) -> nd(V);
 insert(V, N = #node{val=nil}) -> N#node{val=V};
 insert(V, N) when V < N#node.val -> N#node{left=insert(V, N#node.left)};
 insert(V, N) when V >= N#node.val -> N#node{right=insert(V, N#node.right)}.
+
+loop() -> loop(nd(nil)).
+loop(Node) ->
+    io:format("~p\n", [Node]),
+    {ok, [V]} = io:fread("new value: ","~d"),
+    case V of
+        0 -> ok;
+        V -> New = insert(V, Node),
+             loop(New)
+    end.
