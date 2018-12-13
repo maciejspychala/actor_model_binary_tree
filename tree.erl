155bfac0b2a858ca5df0116184ead5c1a57b3798
maciejspychala <mac.spychala@gmail.com>
2018-12-13 18:28:26 +0000

    add message record

--- tree.erl
+++ tree.erl
@@ -4,29 +4,31 @@
 -record(node, {val=nil,
                left=nil,
                right=nil}).
+-record(msg, {type,
+              val}).
 
 nd() -> nd(#node{}).
 nd(Node) ->
     receive
-        {insert, V} ->
-            New = insert(V, Node),
+        M = #msg{type=insert} ->
+            New = insert(M, Node),
             io:format("~p\n", [New]),
             nd(New)
     end.
 
-insert(V, N = #node{val=nil}) ->
-    N#node{val=V};
-insert(V, N) when V < N#node.val ->
-    N#node{left=send_insert(V, N#node.left)};
-insert(V, N) when V >= N#node.val ->
-    N#node{right=send_insert(V, N#node.right)}.
+insert(M, N = #node{val=nil}) ->
+    N#node{val=M#msg.val};
+insert(M, N) when M#msg.val < N#node.val ->
+    N#node{left=send_insert(M, N#node.left)};
+insert(M, N) when M#msg.val >= N#node.val ->
+    N#node{right=send_insert(M, N#node.right)}.
 
-send_insert(V, nil) ->
+send_insert(M, nil) ->
     New = spawn(fun() -> nd() end),
-    New!{insert,V},
+    New!M,
     New;
-send_insert(V, N) ->
-    N!{insert,V},
+send_insert(M, N) ->
+    N!M,
     N.
 
 loop() ->
@@ -36,6 +38,6 @@ loop(Node) ->
     {ok, [V]} = io:fread("new value: ","~d"),
     case V of
         0 -> ok;
-        V -> Node!{insert,V},
+        V -> Node!#msg{type=insert, val=V},
              loop(Node)
     end.
